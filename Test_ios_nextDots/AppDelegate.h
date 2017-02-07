0cda6836532b45acb8259c6244830be8c3895cdf
LTSSetup <brunitoz@gmail.com>
2017-02-07 15:49:20 -0500

    Initial Commit

---
+++ Test_ios_nextDots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_ios_nextDots
+//
+//  Created by LTSSetup on 2/7/17.
+//  Copyright © 2017 BrunoCardenasTorres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_ios_nextDots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_ios_nextDots
+//
+//  Created by LTSSetup on 2/7/17.
+//  Copyright © 2017 BrunoCardenasTorres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
